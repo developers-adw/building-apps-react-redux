5b09b5cde2d1a4e4fa1c6083db2706eab3741d73
Lopez, Richard (Contractor) <richard.lopez@yum.com>
2021-09-24 14:57:10 -0500

    Module 10, dispatch create and update

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { loadCourses } from '../../redux/actions/courseActions';
+import { loadAuthors } from '../../redux/actions/authorActions';
+import PropTypes from 'prop-types';
+import CourseForm from './CourseForm';
+import { newCourse } from '../../../tools/mockData';
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((err) => {
+        alert('Loading courses failed' + err);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((err) => {
+        alert('Loading authors failed' + err);
+      });
+    }
+  }, []);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === 'authorId' ? parseInt(value, 10) : value,
+    }));
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    course: newCourse,
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
